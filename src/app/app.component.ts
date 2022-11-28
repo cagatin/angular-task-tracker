fc3f5ffc17d7c4b44f0676afb94bd81752e8968c
Felix Cagatin <cagatingilbert@gmail.com>
2022-11-28 13:46:51 -0800

    Added comments to app.componet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; // import Component
 
+// Declare component
 @Component({
+  // defines the tag for the component
   selector: 'app-root',
+  // html we use for the component
   templateUrl: './app.component.html',
+  // styles for the component
   styleUrls: ['./app.component.css']
 })
+
+// Here, we define the properties and methods of the component. 
 export class AppComponent {
-  title = 'angular-task-tracker';
+  title: string = 'angular-task-tracker';
 }
