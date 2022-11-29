4a74380ffa83e9f4380e8ce59829703dc5fd3e88
Felix Cagatin <cagatingilbert@gmail.com>
2022-11-29 11:39:10 -0800

    created structure for task-item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
   // Components go here.
@@ -12,7 +13,8 @@ import { TasksComponent } from './components/tasks/tasks.component';
     AppComponent,
     HeaderComponent,
     ButtonComponent,
-    TasksComponent
+    TasksComponent,
+    TaskItemComponent
   ],
   // Modules go here.
   imports: [
