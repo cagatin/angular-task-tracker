38604bac465cc8bab6401568be3bfc92637a45b3
Felix Cagatin <cagatingilbert@gmail.com>
2022-12-02 13:23:05 -0800

    Added toggle event to add button

--- src/app/services/ui.service.ts
+++ src/app/services/ui.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class UiService {
+  private showAddTask: boolean = false;
+  private subject: Subject<any> = new Subject<any>();
+
 
   constructor() { }
+
+  toggleAddTask(): void {
+    this.showAddTask = !this.showAddTask;
+    this.subject.next(this.showAddTask);
+  }
+
+  // method which fires when we toggle the addTask button
+  onToggle(): Observable<any> {
+    return this.subject.asObservable();
+  }
 }
