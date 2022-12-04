86994b89ab8a81f4f505261a4383b8cffbf35928
Gil <Cagagtingilbert@gmail.com>
2022-12-04 10:48:34 -0800

    Added comments to the task service file

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -8,23 +8,38 @@ import { Task } from '../Task';
 })
 
 export class TaskService {
+  // Url for the mock API
   private apiUrl = 'http://localhost:5000/tasks';
+
+  // object containing METHOD options (in this instance, specifies required HTTP Headers)
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   }
 
+  /* Constructors in angular are used to inject dependencies into the Object.
+   * In this insatance, the HttpClient object is being injected into the TaskService service object. 
+   */
   constructor(private httpClient: HttpClient) { }
 
 
+  /* CRUD OPERATIONS
+   * Components will subscribe to following methods which return an Observable Object.
+   * These Observables are primarily tasked with delivering values to the Subject (Component).
+   * Components will SUBSCRIBE to these methods to wait for notifications to recieve values
+   */
+
+  // Creates a GET request to the mock API backend.
   getTasks(): Observable<Task[]> {
     return this.httpClient.get<Task[]>(this.apiUrl);
   }
 
+  // Creates a DELETE request to the mock API backend.
   deleteTask(task: Task): Observable<Task> {
     const url = `${this.apiUrl}/${task.id}`
     return this.httpClient.delete<Task>(url);
   }
 
+  // Creates a PUT request to the mock API backend.
   updateTask(task: Task): Observable<Task> {
     const url = `${this.apiUrl}/${task.id}`;
     return this.httpClient.put<Task>(
@@ -34,6 +49,7 @@ export class TaskService {
     );
   }
 
+  // Creates a POST request to the mock API backend.
   addTask(task: Task): Observable<Task> {
     return this.httpClient.post<Task>(
       this.apiUrl,
