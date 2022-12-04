b006cf3f82c86e57824a5f0b437ab86b64987766
Gil <Cagagtingilbert@gmail.com>
2022-12-04 11:31:06 -0800

    added comments to the task-item component

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -19,10 +19,13 @@ export class TaskItemComponent implements OnInit {
   }
 
   // Function to handle faTimes click event 
+  // Emits the task bound to onDeleteTask event emitter to the parent task-component to delete the task
   onDelete(task: Task) {
     this.onDeleteTask.emit(task);
   }
 
+  // Function to handle double click venet on the specific task
+  // Emits the task bound to onToggleReminder to the parent task-component to change the reminder border
   onToggle(task: Task) {
     this.onToggleReminder.emit(task);
   }
