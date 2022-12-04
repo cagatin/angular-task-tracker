76678d3512188fc64ba98326e67559a0ada7869d
Gil <Cagagtingilbert@gmail.com>
2022-12-04 11:07:00 -0800

    Added comments to the Header Component file

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,11 +8,15 @@ import { Router } from '@angular/router'
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent implements OnInit {
   title: string = 'Task Tracker';
-  showAddTask!: boolean;
-  subscription!: Subscription;
+  showAddTask!: boolean;            // tracks the current state of the 'Add' task button
+  subscription!: Subscription;      // used to subscribe to the uiService service object.
 
+  /* Constructors in angular are used to inject dependencies into the Object.
+   * In this insatance, the UiService object and Router object are being injected into the Header component
+   */
   constructor(private uiService: UiService, private router: Router) {
     this.subscription = this.uiService.onToggle().subscribe((value) => this.showAddTask = value);
   }
@@ -20,10 +24,12 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // calls the UI Service object's toggleAddTask to toggle the state of the 'Add' button
   toggleAddTask() {
     this.uiService.toggleAddTask();
   }
 
+  // Method to check if the current route matches the route passed in as a parameter.
   hasRoute(route: string): boolean {
     return this.router.url === route;
   }
