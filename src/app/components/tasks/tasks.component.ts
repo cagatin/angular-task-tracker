9d8521482c9722bc085608283a69f37a687c0a1a
Gil <Cagagtingilbert@gmail.com>
2022-12-04 11:22:53 -0800

    Added comments to the tasks component ts file

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -8,14 +8,18 @@ import { TaskService } from 'src/app/services/task.service';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = [];
+  tasks: Task[] = [];   // array containing Task objects
 
+  // Injects the taskService service object as a dependency into the Task Component object
   constructor(private taskService: TaskService) { }
 
+  // When the component is first rendered, retrieve the tasks from the task service object and bind them to the component's Task[] array
   ngOnInit(): void {
     this.taskService.getTasks().subscribe((task) => this.tasks = task);
   }
 
+  // Method which takes in a task object and deletes it.
+  // Filters the task array to remove the specified task.
   deleteTask(task: Task): void {
     this.taskService
       .deleteTask(task)
@@ -24,12 +28,15 @@ export class TasksComponent implements OnInit {
       );
   }
 
+  // Method used to toggle the reminder property of the task object.
+  // Updates the task using the taskService obejct
   toggleReminder(task: Task): void {
     task.reminder = !task.reminder;
     this.taskService.updateTask(task).subscribe();
   }
 
 
+  // Adds a task
   addTask(task: Task): void {
     this.taskService.addTask(task).subscribe((task) => this.tasks.push(task))
   }
